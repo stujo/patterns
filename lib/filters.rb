e7c901d277c99ce6a039af2d3be8734bdaa08b84
macournoyer <macournoyer@gmail.com>
2013-07-30 13:37:34 -0400

    Implement around_action.

--- lib/filters.rb
+++ lib/filters.rb
@@ -4,26 +4,70 @@ module Filters
   end
 
   module ClassMethods
-    def before_actions
-      @before_actions ||= []
-    end
-
     def before_action(method)
-      before_actions << method
-    end
-    
-    def after_actions
-      @after_actions ||= []
+      around_action do |controller, action|
+        controller.send method
+        action.call
+      end
     end
 
     def after_action(method)
-      after_actions << method
+      around_action do |controller, action|
+        action.call
+        controller.send method
+      end
+    end
+    
+    def around_actions
+      @around_actions ||= []
+    end
+
+    # around_action :layout
+    #
+    # def layout
+    #   yield
+    # end
+    #
+    #  or
+    #
+    # around_action do |controller, action|
+    #   controller.response.write "..."
+    #   action.call
+    # end
+    def around_action(method=nil, &block)
+      if block
+        around_actions << block
+      else # method
+        around_actions << proc { |controller, action| controller.send method, &action }
+      end
     end
   end
 
   def process(action)
-    self.class.before_actions.each { |method| send method }
-    super
-    self.class.after_actions.each { |method| send method }
+    # around_action :one
+    # around_action :two
+    #
+    # def one
+    #   yield
+    # end
+    # def two
+    #   yield
+    # end
+    #
+    # action_proc = proc do
+    #   one do
+    #     two do
+    #       super
+    #     end
+    #   end
+    # end
+
+    action_proc = proc { super }
+    self.class.around_actions.reverse.each do |block|
+      current_action = action_proc
+      action_proc = proc { block.call(self, current_action) }
+    end
+
+    action_proc.call
   end
 end
