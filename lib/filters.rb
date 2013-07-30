5e00605bd0880cead7f857f1943f62e55ad7a9d7
macournoyer <macournoyer@gmail.com>
2013-07-30 12:58:14 -0400

    Implement after_action.

--- lib/filters.rb
+++ lib/filters.rb
@@ -11,10 +11,19 @@ module Filters
     def before_action(method)
       before_actions << method
     end
+    
+    def after_actions
+      @after_actions ||= []
+    end
+
+    def after_action(method)
+      after_actions << method
+    end
   end
 
   def process(action)
     self.class.before_actions.each { |method| send method }
     super
+    self.class.after_actions.each { |method| send method }
   end
 end
