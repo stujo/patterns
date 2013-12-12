ce6309b4a94d88a32ff8ed848953745abb81b412
macournoyer <macournoyer@gmail.com>
2013-12-12 11:48:57 -0500

    Implement routing DSL.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Routes = Router.new do
   match '/' => 'home#index'
+  match '/hi' => 'users#index'
   match '/home/index' => 'home#index'
   match '/echo' => 'echo#index'
   match '/users' => 'users#index'
